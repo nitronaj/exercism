c346cfc4c3e48af84c28a62ef95a359690c4a6ac
Charlie Nader <nitro.naj@gmail.com>
2024-07-18 15:08:11 -0700

    Add bob solution, inspired from community code

--- go/bob/bob.go
+++ go/bob/bob.go
@@ -10,44 +10,48 @@ import (
 	"unicode"
 )
 
+type Remark string
+
+func NewRemark(remark string) Remark {
+	return Remark(strings.TrimSpace(remark))
+}
+
+func (r *Remark) String() string {
+	return string(*r)
+}
+func (r *Remark) isQuestion() bool {
+	return strings.HasSuffix(r.String(), "?")
+}
+
+func (r *Remark) isYelling() bool {
+	hasAlphabet := strings.IndexFunc(r.String(), unicode.IsLetter) >= 0
+	isUpperCase := strings.ToUpper(r.String()) == r.String()
+
+	return hasAlphabet && isUpperCase
+}
+
+func (r *Remark) isYellingQuestion() bool {
+	return r.isYelling() && r.isQuestion()
+}
+
+func (r *Remark) isSilence() bool {
+	return *r == ""
+}
+
 // Hey should have a comment documenting it.
 func Hey(remark string) string {
-	remark = strings.TrimSpace(remark)
+	r := NewRemark(remark)
 
 	switch {
-	case isQuestion(remark) && isYelling(remark):
+	case r.isYellingQuestion():
 		return "Calm down, I know what I'm doing!" // This is what he says if you yell a question at him.
-	case isQuestion(remark):
+	case r.isQuestion():
 		return "Sure." //   The convention used for questions is that it ends with a question mark.
-	case isYelling(remark): // The convention used for yelling is ALL CAPITAL LETTERS.
+	case r.isYelling(): // The convention used for yelling is ALL CAPITAL LETTERS.
 		return "Whoa, chill out!"
-	case isSilence(remark):
+	case r.isSilence():
 		return "Fine. Be that way!" // This is how he responds to silence. The convention used for silence is nothing, or various combinations of whitespace characters.
 	default:
 		return "Whatever."
 	}
-}
-
-func isQuestion(str string) bool {
-	return str != "" && string(str[len(str)-1]) == "?"
-}
-
-func isYelling(str string) bool {
-	hasAlphabet := false
-	for _, char := range str {
-		if unicode.IsLetter(char) {
-			if !unicode.IsUpper(char) {
-				return false
-			}
-
-			hasAlphabet = true
-		}
-	}
-
-	return hasAlphabet
-}
-
-func isSilence(str string) bool {
-	return len(str) == 0
-
 }
