8895a445b6081e7d182f22d065cb348219b11e69
Charlie Nader <nitro.naj@gmail.com>
2024-07-22 15:30:33 -0700

    simpler calculation for rotational cipher

--- go/rotational-cipher/rotational_cipher.go
+++ go/rotational-cipher/rotational_cipher.go
@@ -8,15 +8,16 @@ func ShiftChar(char rune, shiftKey int) rune {
 	newChar := int(char) + shiftKey
 
 	if unicode.IsLower(char) && int(newChar) > 122 {
-		newChar = newChar - int('z') - 1 + int('a')
+		newChar = newChar - 26 // int('z') - 1 + int('a')
 	} else if unicode.IsUpper(char) && int(newChar) > 90 {
-		newChar = newChar - int('Z') - 1 + int('A')
+		newChar = newChar - 26 // int('Z') - 1 + int('A')
 	}
 	return rune(newChar)
 }
 
 func RotationalCipher(plain string, shiftKey int) string {
 	var result string
+
 	for _, char := range plain {
 		if unicode.IsLetter(char) {
 			result += string(ShiftChar(char, shiftKey))
