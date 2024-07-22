9f103e30053ef4f4fcfa22031eac38731da453f0
Charlie Nader <nitro.naj@gmail.com>
2024-07-22 16:07:18 -0700

    Adding simpler solution for resister color

--- go/resistor-color-duo/resistor_color_duo.go
+++ go/resistor-color-duo/resistor_color_duo.go
@@ -1,27 +1,19 @@
 package resistorcolorduo
 
-import "math"
+var mapColors = map[string]int{
+	"black":  0,
+	"brown":  1,
+	"red":    2,
+	"orange": 3,
+	"yellow": 4,
+	"green":  5,
+	"blue":   6,
+	"violet": 7,
+	"grey":   8,
+	"white":  9,
+}
 
 // Value should return the resistance value of a resistor with a given colors.
 func Value(colors []string) int {
-
-	mapColors := map[string]int{
-		"black":  0,
-		"brown":  1,
-		"red":    2,
-		"orange": 3,
-		"yellow": 4,
-		"green":  5,
-		"blue":   6,
-		"violet": 7,
-		"grey":   8,
-		"white":  9,
-	}
-	resistance := 0
-
-	for i := 0; i < len(colors) && i < 2; i++ {
-		resistance += mapColors[colors[i]] * int(math.Pow10(1-i))
-	}
-
-	return resistance
+	return mapColors[colors[0]]*10 + mapColors[colors[1]]
 }
